dd8befecbead8faa2c59318fe036cc4580f3cc63
rvats <vats.rahul@gmail.com>
2016-05-11 19:51:59 -0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Algorithms;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "";
+            long square = 0;
+            Console.Write("Please Enter Number To Square: ");
+            line = Console.ReadLine();
+            square = ProkarmaProvidence.Square(line);
+            Console.WriteLine("Square of " + line + " is " + square.ToString());
+            
+            /* Online Test Question 1
+             * Input will be a string followed by Another String separated by a comma we have to find if the 
+             * string ends with substring followed after ,
+             */
+            Console.WriteLine("Please Enter A String followed by a comma followed by another string to test the condition:");
+            line = Console.ReadLine();
+            if(ProkarmaProvidence.SubStringTest(line))
+            {
+                Console.WriteLine("String ends in SubString");
+            }
+            else
+            {
+                Console.WriteLine("String does not end in SubString");
+            }
+
+            /* Online Test Question 2
+             * Input will be a url, separate the protocol, domain and query from url into output
+             */
+
+            /* Online Test Question 3
+             * Check for formatted string made of (){}[]
+             */
+
+            /* Online Test Question 4 Could not Attempt */
+
+            /* Some Delay to See The Final Results */
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ ProkarmaProvidence.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class ProkarmaProvidence
+    {
+        public static long Square (string line)
+        {
+            int number = 0;
+            long square = 0;
+            try
+            {
+                if (int.TryParse(line, out number))
+                {
+                    square = number * number;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter a Valid Integer");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return square;
+        }
+
+        /*Function to solve the first problem*/
+        public static bool SubStringTest(string line)
+        {
+            bool result = false;
+            string[] str;
+            try
+            {
+                str = line.Split(',');
+                if(str[0].EndsWith(str[1]))
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+
+        /*Function to solve the second problem - Need to work on the solution. Attempted and solve the problem*/
+        public static bool URLSplit(string line)
+        {
+            bool result = false;
+            string[] str;
+            try
+            {
+                str = line.Split(',');
+                if (str[0].EndsWith(str[1]))
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+
+        /*Function to solve the third problem - Need to work on the solution. Messed up*/
+        public static bool FormattedString(string line)
+        {
+            bool result = false;
+            string[] str;
+            try
+            {
+                str = line.Split(',');
+                if (str[0].EndsWith(str[1]))
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+    }
+}
